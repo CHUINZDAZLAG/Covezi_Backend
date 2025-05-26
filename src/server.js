e542c5bde8844ca875f9fc291f8173eef57a9472
trander <thevinh15925@gmail.com>
2025-05-26 21:53:50 +0700

    add error handling

--- src/server.js
+++ src/server.js
@@ -5,15 +5,19 @@ import AsyncExitHook from 'async-exit-hook'
 import { CLOSE_DB, CONNECT_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1/index'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
-// hello
 const START_SERVER = () => {
   const app = express()
 
   // Enable req.body json data
   app.use(express.json())
 
+  // Use APIs V1
   app.use('/v1', APIs_V1)
+
+  // Middleware xu ly loi tap trung
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`3. Hi ${env.AUTHOR}, Server is running at ${env.APP_HOST}:${env.APP_PORT}`)
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
-
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -17,10 +17,9 @@ const createNew = async (req, res, next) => {
     // Validation xong thi cho request chay tiep sang Controller
     next()
   } catch (error) {
-    console.log('Error: ', error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
   }
 }
 
